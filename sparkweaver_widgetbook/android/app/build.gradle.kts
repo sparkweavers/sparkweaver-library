9bd7de8b8eef6cef8c25f7e767289ce7e2a2399d
Birgit Pohl <BirgitPohl@users.noreply.github.com>
2026-09-07 09:55:01 +0200

    chore: set the Widgetbook application id and drop the scaffolding TODO

--- sparkweaver_widgetbook/android/app/build.gradle.kts
+++ sparkweaver_widgetbook/android/app/build.gradle.kts
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sparkweaver_widgetbook"
+        applicationId = "com.sparkweaver_widgetbook.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
